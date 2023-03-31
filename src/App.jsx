c1e7098f9ff77e7317bb6ea1375784bd4ebf81b5
AG-code32 <i.gerson@hotmail.com>
2023-03-31 06:53:51 -0500

    nav component finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 
@@ -7,8 +8,17 @@ const App = () => {
     <BrowserRouter>
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar></Navbar>
-          <Hero></Hero>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div>
+          <Contact />
+          <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
